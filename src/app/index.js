8b2b3b9f25567a57b9680d78262f3fa6b8d61b8f
aliza8833 <alizaaslam8833@gmail.com>
2024-07-11 00:03:27 +0500

    changes commited

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,35 @@
 
-import { StyleSheet, Text, View,FlatList } from 'react-native';
+import { StyleSheet, Text, View,FlatList,Alert } from 'react-native';
 import{Link} from 'expo-router'
 import {Stack} from 'expo-router';
 import { AntDesign } from '@expo/vector-icons';
+import {useState,useEffect} from 'react';
+import {supabase} from '../lib/supabase';
 const polls =[{id:1},{id:2},{id:3}];
 export default function HomeScreen() {
+  const [polls,setPolls]=useState([]);
+  useEffect(()=>{
+    const fetchPolls = async() => {
+      console.log('Fetching...');
+      
+let { data , error } = await supabase
+.from('polls')
+.select('*')
+   if(error)  {
+    Alert.alert('Error fetching data');
+   } 
+   console.log(data); 
+   setPolls(data); 
+    };
+    fetchPolls();
+
+  },[]);
   return (
+
     <>
-    <Stack.Screen options={{title:'Polls', headerTitleAlign:'center',headerRight:()=>(<Link href={"/polls/new"} ><AntDesign name="plus" size={20} color="gray" /></Link>),}}/>
+    <Stack.Screen 
+    options={{title:'Polls', headerTitleAlign:'center',
+headerRight:()=>(<Link href={"/polls/new"} ><AntDesign name="plus" size={20} color="gray" /></Link>),}}/>
 <FlatList
 data={polls}
 contentContainerStyle={styles.container}
